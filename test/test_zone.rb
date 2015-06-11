6494c84210c2333a5fa54b49724b85f883b5da2d
Tobias Böhm <aibo@aibor.de>
2015-06-11 13:32:59 +0200

    added some more assertions

--- test/test_zone.rb
+++ test/test_zone.rb
@@ -2,11 +2,21 @@ require 'init_test_env'
 
 class TestZone < Minitest::Test
   def setup
-    @zones = [{}]
     base_path = '/servers/localhost'
+    @zone = Zone.new id: 'example.com',
+      name: 'example.com',
+      type: 'Zone',
+      kind: 'Master',
+      url: "#{base_path}/zones/example.com",
+      dnssec: true
+
+    @zones = [@zone]
+
     ActiveResource::HttpMock.respond_to do |mock|
       mock.get "#{base_path}/zones", Zone.headers, @zones.to_json
-      mock.get "#{base_path}/zones/1", Zone.headers, @zones.first.to_json
+      mock.get @zone.url, Zone.headers, @zone.to_json
+      mock.get "#{base_path}/zones/not_existent.com", Zone.headers, nil, 404
+      mock.delete @zone.url, Zone.headers, nil, 204
     end
   end
 
@@ -15,7 +25,15 @@ class TestZone < Minitest::Test
   end
 
   def test_get
-    assert Zone.get(1)
+    assert Zone.get('example.com')
+    assert_raises(ActiveResource::ResourceNotFound) do
+      Zone.find('not_existent.com')
+    end
+  end
+
+  def test_delete
+    assert Zone.find('example.com').destroy
   end
 
 end
+
